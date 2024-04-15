b64ce7693aeebe8a60de99849bebc7eab32e014b
Xianshun Song <152236102+XianshunSong@users.noreply.github.com>
2024-04-15 10:06:39 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,7 +1,5 @@
 `default_nettype none `timescale 1ns / 1ps
 
-module johnson_test();
-
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
