ffb0e9da94a67fc02559db561ae01de67b667968
Xianshun Song <152236102+XianshunSong@users.noreply.github.com>
2024-04-15 10:21:18 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -21,6 +21,13 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
+  reg reset;
+  reg we;   
+  reg [2:0] a1;
+  reg [7:0] wd;
+  reg [2:0] wa;
+  wire [7:0] rd1;
+
   // Replace tt_um_example with your module name:
   tt_um_opt_encryptor user_project (
 
@@ -40,13 +47,39 @@ module tb ();
       .rst_n  (rst_n)     // not reset
   );
 
+    register_file rf (
+        .reset(reset),
+        .clock(clk),
+        .we(we),
+        .a1(a1),
+        .wd(wd),
+        .wa(wa),
+        .rd1(rd1)
+    );   
 
+
+    //clock
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk; // Generate a clock with a period of 10 ns
+    end
+   
 initial begin
-      clk=1;
-      ui_in[0]=0;
-      #5 ui_in[0]=1;
-      #5 ui_in[0]=0;
-      #20 $finish;
+      reset = 1; we = 0; a1 = 0; wd = 0; wa = 0; ui_in = 0; uio_in = 0; ena = 0; rst_n = 0;
+      #10;
+      reset = 0; rst_n = 1;
+
+      we = 1; wa = 3'd5; wd = 8'hAA;
+      #10;
+      we = 0; a1 = 3'd5;
+
+      ena = 1; ui_in = 8'hFF; uio_in = 8'b00000010; // simulate encryption request
+      #20;
+
+      //decryption
+      ui_in = 8'h00; uio_in = 8'b10000010; // simulate decryption request
+   
+      #100 $finish;
 end
 
 initial $monitor($time,,"[%b]\t%b\tq=%b",clk,ui_in[0],uo_out);
