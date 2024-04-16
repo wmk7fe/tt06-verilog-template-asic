d042f5b6705175b3704b7aa18e1e884ce7ee8e3e
wmk7fe <112506007+wmk7fe@users.noreply.github.com>
2024-04-16 01:31:47 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -13,16 +13,14 @@ async def test_otp_encryptor(dut):
     ena = dut.ena
     data_in = dut.ui_in
     data_out = dut.uo_out
-    decrypt = dut.uio_in[0]
-    rnum_in = dut.uio_in[3:1]
-    rnum_out = dut.uio_out[6:4]
+    rnum_decrypt_in = dut.uio_in
+    rnum_out = dut.uio_out
     rst_n = dut.rst_n
     
     rst_n.value = Force(1)
     ena.value = Force(0)
     data_in.value = Force(0)
-    decrypt.value = Force(0)
-    rnum_in.value = Force(0)
+    rnum_decrypt_in.value = Force(0)
 
     await(ClockCycles(clk, 1))
 
@@ -36,9 +34,9 @@ async def test_otp_encryptor(dut):
 
     data_in.value = Force(0xab)
     ena.value = Force(1)
-    await(ClockCycles(dut.clk, 1))
+    await(ClockCycles(clk, 1))
 
     ena.value = Force(0)
     data = data_out.value
-    rnum = rnum_out.value
+    rnum = rnum_out.value >> 4
     dut._log.info(f'Encrypted output: {data} ({rnum})')
