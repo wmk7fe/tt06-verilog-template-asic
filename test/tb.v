73c2e7d0c797a609e05ed752e5de0c62698a979d
wmk7fe <112506007+wmk7fe@users.noreply.github.com>
2024-04-16 13:46:05 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -10,16 +10,23 @@ module tb ();
     $dumpvars(0, tb);
     #1;
   end
-
+   
   // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
+  reg [3:0] uio_in_upper;
   wire [7:0] uo_out;
-  wire [7:0] uio_out;
+  wire uio_out_upper;
+  wire [3:0] uio_out_lower; 
   wire [7:0] uio_oe;
+
+  reg [7:0] data;
+  reg [2:0] r_num;
+  reg decrypt;
+
+  wire [7:0] out;
+  wire [2:0] index_out;
 
   // Replace tt_um_example with your module name:
   tt_um_opt_encryptor user_project (
@@ -30,10 +37,10 @@ module tb ();
       .VGND(1'b0),
 `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
+      .ui_in  (data),    // Dedicated inputs
+      .uo_out (out),   // Dedicated outputs
+      .uio_in ({uio_in_upper, r_num, decrypt}),   // IOs: Input path
+      .uio_out({uio_out_upper, index_out, uio_out_lower}),  // IOs: Output path
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
@@ -41,32 +48,8 @@ module tb ();
   );
 
 
-    //clock
-    initial begin
-        clk = 0;
-        forever #5 clk = ~clk; // Generate a clock with a period of 10 ns
-    end
-   
-initial begin
-        // Initialize all signals
-        rst_n = 0; ena = 0; ui_in = 0; uio_in = 0;
-        #10;
-        rst_n = 1;  // Release reset
+   initial $monitor($time,,"[%b]\t%b\tq=%b || data_in %b, data_out %b, index_in %b, index_out %b, enable %b, decrypt enable %b, clk %b",clk,ui_in[0],uo_out, data, out, r_num, index_out, ena, decrypt, clk);
 
-        // encryption
-        ena = 1; ui_in = 8'hFF; uio_in = 8'b00000010; // Set up encryption
-        #20;
-
-        //decryption
-        ui_in = 8'h00; uio_in = 8'b10000010; // Set up decryption
-
-        #100;
-        $finish; // End simulation after 100 ns
-end
-
-initial $monitor($time,,"[%b]\t%b\tq=%b",clk,ui_in[0],uo_out);
-
-always #2 clk=~clk;
    
 endmodule
 
