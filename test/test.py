b4c7abe6049331e4c89b091660085a0ef84b27fb
wmk7fe <112506007+wmk7fe@users.noreply.github.com>
2024-04-16 00:01:04 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -9,6 +9,11 @@ async def test_otp_encryptor(dut):
     clock = Clock(dut.clk, 10, units="ns")  # Clock period of 10 ns
     cocotb.start_soon(clock.start())
 
+    # start alex's changes, ensure that there is a falling edge to trigger reset
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 5)
+    #end alex's changes
+    
     # Reset the device
     dut.rst_n.value = 0
     dut.ena.value = 0
@@ -22,17 +27,33 @@ async def test_otp_encryptor(dut):
     # Set up for encryption
     dut.ena.value = 1
     dut.ui_in.value = 0xFF  # Example plaintext
-    dut.uio_in.value = BinaryValue("00000010")  # Encryption setup
+
+    # start alex's changes, comment value and replace
+    # dut.uio_in.value = BinaryValue("00000010")  # Encryption setup
+    dut.uio_in.value = BinaryValue("00000000") 
+    # end alex's changes (this is unnecessary but rnum is only an input for decryption, cannot manually set register to be stored in)
+    
     await ClockCycles(dut.clk, 2)
 
     # Capture the output from encryption
     encrypted_value = dut.uo_out.value
     dut._log.info(f'Encrypted output: {encrypted_value}')
 
+    # start alex's changes, save register number
+    rout = (dut.uio_out.value & 0x70) >> 4
+    dut._log.info(f'Register number: {rout}')
+    # end alex's changes
+
     # Scenario: Decryption
     # Assuming the device can decrypt its own output
     dut.ui_in.value = encrypted_value  # Feed the encrypted value as input
-    dut.uio_in.value = BinaryValue("10000010")  # Decryption setup
+
+    # start alex's changes - don't drive uio pins 4-7 as they are output pins
+    # instead, drive pins 0-3 with 1 for decrypt for bit 0 and bits 1-3 to match the register num
+    # dut.uio_in.value = BinaryValue("10000010")  # Decryption setup
+    dut_uio_in.value = (0xFF & ((rnum << 1) + 1))
+    # end alex's changes
+    
     await ClockCycles(dut.clk, 2)
 
     # Check if the decrypted output matches the original input (0xFF)
