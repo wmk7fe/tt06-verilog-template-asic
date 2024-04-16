fcf069c19400ae7773b1e5ed78f7843375d3fc9e
wmk7fe <112506007+wmk7fe@users.noreply.github.com>
2024-04-16 13:09:29 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,78 +1,85 @@
 import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles
-from cocotb.binary import BinaryValue
-from cocotb.handle import Force
+from cocotb.triggers import FallingEdge, Timer
 
+
+async def generate_clock(dut):
+    """Generate clock pulses."""
+
+    for cycle in range(100):
+        dut.clk.value = 0
+        await Timer(1, units="ns")
+        dut.clk.value = 1
+        await Timer(1, units="ns")
 
 
 @cocotb.test()
-async def test_otp_encryptor(dut):
+async def test_otp_encyrptor(dut):
+    """Try accessing the design."""
+
     clk = dut.clk
-    clock = Clock(clk, 10, units="ns")
-    
     ena = dut.ena
     data_in = dut.ui_in
     data_out = dut.uo_out
     rnum_decrypt_in = dut.uio_in
     rnum_out = dut.uio_out
     rst_n = dut.rst_n
+
+    rst_n.value = 1
+    ena.value = 0
+    data_in.value = 0
+    rnum_decrypt_in.value = 0
     
-    rst_n.value = Force(1)
-    ena.value = Force(0)
-    data_in.value = Force(0)
-    rnum_decrypt_in.value = Force(0)
+    await cocotb.start(generate_clock(dut))  # run the clock "in the background"
 
-    await(ClockCycles(clk, 1))
+    await Timer(5, units="ns")  # wait a bit
+    await FallingEdge(dut.clk)  # wait for falling edge/"negedge"
 
-    rst_n.value = Force(0)
+    rst_n.value = 0
 
-    await(ClockCycles(clk, 1))
+    await FallingEdge(dut.clk)
 
-    rst_n.value = Force(1)
+    rst_n.value = 1
 
-    await(ClockCycles(clk, 1))
+    await FallingEdge(clk)
 
-    data_in.value = Force(0xab)
-    ena.value = Force(1)
+    data_in.value = 0xab
+    ena.value = 1
     
-    await(ClockCycles(clk, 1))
+    await FallingEdge(clk)
 
     dut._log.info(f'Encryptor  input: {data_in.value}')
 
-    await(ClockCycles(clk, 1))
+    await FallingEdge(clk)
     
     data = data_out.value
     rnum = rnum_out.value >> 4
     dut._log.info(f'Encrypted output: {data_out.value} ({rnum_out.value >> 4})')
 
-    await(ClockCycles(clk,1))
+    await FallingEdge(clk)
 
     decrypt = 1
     data_in.value = Force(0)
     rnum_decrypt_in.value = Force((rnum << 1) + decrypt)
 
-    await(ClockCycles(clk, 1))
+    await FallingEdge(clk)
 
     dut._log.info(f'Decryptor  input: {data_in.value} ({rnum_decrypt_in.value})')
 
-    await(ClockCycles(clk, 1))
+    await FallingEdge(clk)
 
     data = data_out.value
     dut._log.info(f'stored pad value: {data_out.value}')
 
-    await(ClockCycles(clk, 1))
+    await FallingEdge(clk)
 
     data_in.value = Force(data)
     rnum_decrypt_in.value = Force((rnum << 1) + decrypt)
 
-    await(ClockCycles(clk, 1))
+    await FallingEdge(clk)
 
     dut._log.info(f'Decryptor  input: {data_in.value} ({rnum_decrypt_in.value})')
 
-    await(ClockCycles(clk, 1))
+    await FallingEdge(clk)
 
     data = data_out.value
     dut._log.info(f'stored pad value: {data_out.value}')
-
-    
